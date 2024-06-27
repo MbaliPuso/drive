6b4891d1d8c523f38e745c49707ae2310914edf0
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-06-28 01:43:40 +0200

    route setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AllCars from './pages/AllCars';
+import ApplyNow from './pages/ApplyNow';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/browse-cars' element={<AllCars />} />
+        <Route path='/apply-now' element={<ApplyNow />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
