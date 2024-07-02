58a7a5f7d1f6d60478afe311c7918b8324f9b13a
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-02 16:50:04 +0200

    updated cards

--- src/components/FeaturedVehicles.jsx
+++ src/components/FeaturedVehicles.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import carOne from "../assets/suzuki-swift.png";
@@ -14,7 +14,8 @@ const FeaturedVehicles = () => {
             petrolConsumption: '4.9L/100km',
             engineCapacity: '1.2L',
             offers: 'Insurance, Maintenance, Services, Tracking, Tyres, Roadside Assist',
-            price: 'R7,299'
+            price1500km: 'R7,299',
+            price2500km: 'R7,899'
         },
         {
             image: carTwo,
@@ -23,7 +24,8 @@ const FeaturedVehicles = () => {
             petrolConsumption: '6.9L/ 100km',
             engineCapacity: '1.5L',
             offers: 'Insurance, Maintenance, Services, Tracking, Tyres, Roadside Assist',
-            price: 'R12,699'
+            price1500km: 'R12,699',
+            price2500km: 'R14,299'
         },
         {
             image: carThree,
@@ -32,15 +34,27 @@ const FeaturedVehicles = () => {
             petrolConsumption: '6.8L/ 100km',
             engineCapacity: '1.5L',
             offers: 'Insurance, Maintenance, Services, Tracking, Tyres, Roadside Assist',
-            price: 'R12,199'
+            price1500km: 'R13,499',
+            price2500km: 'R14,499'
         },
     ];
+
+    const [selectedPrices, setSelectedPrices] = useState(cars.map(car => car.price1500km));
     
+    const handleKilometersClick = (index, kilometers) => {
+        const newPrices = [...selectedPrices];
+        newPrices[index] = kilometers === 1500 ? cars[index].price1500km : cars[index].price2500km;
+        setSelectedPrices(newPrices);
+    };
+
     return (
-        <div className="container featured-vehicles">
-        <h2>Featured Vehicles</h2>
-        {cars.map((car, index) => (
-            <div key={index} className="col-lg-3 col-md-12 mb-4 car">
+        <>
+        <h3 className='featured-vehicles'>Featured Vehicles</h3>
+        <div className="container">
+            <div className='row'>
+
+            {cars.map((car, index) => (
+                <div key={index} className="col-lg-4 col-md-12 mb-4 car">
                 <div className='card h-100'>
                     <img src={car.image} alt={car.name} className='car-image' />
                     <h3 className="car-name">{car.name}</h3>
@@ -49,25 +63,30 @@ const FeaturedVehicles = () => {
                         <div className='col-4'>{car.petrolConsumption}</div>
                         <div className='col-4'>{car.engineCapacity}</div>
                     </div>
-                    <p><strong>Offers Include:</strong>{car.offers}</p>
-                    <div className='row'>
+                    <p className='offers-include'><strong>Offers Include: </strong>{car.offers}</p>
+                    <div className='row apply-kilometers'>
                         <div className='col-6'>
-                            <button type="button" className='btn btn-primary'>Apply Now</button>
+                            <button type="button" className='apply-now-featured'>Apply Now</button>
                         </div>
                         <div className='col-6 monthly-kilometers'>
-                            <p>Monthly listeners</p>
+                            <p>Monthly kilometers</p>
                             <span>
-                               <button>1500km</button>
-                               <button>2500km</button>
+                               <button className={setSelectedPrices[index] === car.price1500km ? 'active' : ''} onClick={() => handleKilometersClick(index, 1500)}>
+                               1500km</button>
+                               <button className={setSelectedPrices[index] === car.price2500km ? 'active' : ''} onClick={() => handleKilometersClick(index, 2500)}>
+                               2500km</button>
                             </span>
                         </div>
                     </div>
-                    <div>{car.price} <span>Per Month</span></div>
+                    <div className='price'>{selectedPrices[index]} <span className='per-month'>Per Month</span></div>
                 </div>
             </div>
             ))}
-            <button type="button" className='btn btn-outline-success view-all-button'><Link to="/browse-cars">View All Cars</Link></button>
+            <button type="button" className='btn btn-outline-success view-all-button'><Link to="/browse-cars">View All Cars »</Link></button>
+
+            </div>
         </div>
+        </>
     );
 };
 
