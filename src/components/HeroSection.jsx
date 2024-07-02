da7e53a5d2371dab35eeb06d6f4a8008a6e5b8dd
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-02 16:50:22 +0200

    fixed hero images and text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,12 +4,20 @@ import hero from "../assets/suzuki-grand-vitara.jpg";
 import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
+    const heroStyle = {
+        backgroundImage: `url(${hero})`,
+    };
+
     return (
-        <div className='hero-section'>
-            <div className='hero-text' style={{ backgroundImage: `url(${hero})` }}>
-                <h1>THE FUTURE OF DRIVING</h1>
-                <p>Get a brand new car today with just a few clicks - <br />Smart, Quick & Easy - With Drive!</p>
-                <button className='btn btn-warning apply-now'><Link to="/apply-now">Apply Now</Link></button>
+        <div className='hero-section' style={heroStyle}>
+            <div className='container'>
+                <div className='row hero-content'>
+                  <div className='col-lg-6'>
+                  <h1>THE FUTURE OF DRIVING</h1>
+                  <p>Get a brand new car today with just a few clicks - Smart, Quick & Easy - With Drive!</p>
+                  <button className='btn btn-warning apply-now'><Link to="/apply-now">Apply Now</Link></button>
+                  </div>
+                </div>
             </div>
         </div>
     );
