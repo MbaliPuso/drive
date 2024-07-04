2c535065406b87dc9df6e45eaf8468ee73a6af13
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-05 01:30:40 +0200

    changed navbar position

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <button className='navbar-toggler' type='button' data-toggle="collapse" data-target="#navbarNav" aria-controls='navbarNav' aria-expanded="false" aria-label='Toggle navigation'>
         <span className='navbar-toggler-icon'></span>
       </button>
-      <button className="call-now">Call Now</button>
+      <button className="call-now btn btn-warning">Call Now</button>
       <div className='collapse navbar-collapse' id='navbarNav'>
       <ul className='navbar-nav ml-auto'>
         <li className='nav-item'><Link to="/">Home</Link></li>
