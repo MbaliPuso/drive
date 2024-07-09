8d3fb22ed8da6edf0c5a727b73c57b1ac791418d
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-09 14:05:59 +0200

    removed how it works section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,6 @@ import Navbar from "../components/NavigationBar";
 import HeroSection from '../components/HeroSection';
 import FeaturedVehicles from '../components/FeaturedVehicles';
 import QuickApply from '../components/QuickApply';
-import HowItWorks from '../components/HowItWorks';
-import PaymentProsCons from '../components/PaymentProsCons';
 import Footer from '../components/Footer';
 
 const Home = () => {
@@ -14,10 +12,6 @@ const Home = () => {
            <HeroSection />
            <FeaturedVehicles />
            <QuickApply />
-           <section id='how-it-works'>
-            <HowItWorks />
-           </section>
-           <PaymentProsCons />
            <Footer />
         </div>
     );
