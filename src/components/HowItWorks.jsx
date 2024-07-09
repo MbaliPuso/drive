bbffeb8fe31db36d25a5f5181c11c37b7d111463
MbaliPuso <123210276+MbaliPuso@users.noreply.github.com>
2024-07-09 14:05:24 +0200

    removed section

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,68 +1,34 @@
 import React from 'react';
 import '../styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import works from '../assets/bg_2.jpg';
-// import stepOne from '../assets/how-it-works-1.png';
-// import stepTwo from '../assets/how-it-works-2.png';
-// import stepThree from '../assets/how-it-works-3.png';
+import work from '../assets/Chery-Omoda-5.jpg';
 
 const HowItWorks = () => {
-    const worksStyle = {
-        backgroundImage: `url(${works})`,
-    }
-
+  const workStyle = {
+    backgroundImage: `url(${work})`,
+  }
     return (
-        <div className="container how-it-works">
+        <div className="container how-it-works" style={workStyle}>
+            <p className="work-flow">Work Flow</p>
             <h2 className="drive-heading text-center">How Drive Works</h2>
-            <section class="ftco-section services-section img" style={worksStyle}>
-    	<div class="overlay"></div>
-    	<div class="container">
-    		<div class="row justify-content-center mb-5">
-          <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
-          	<span class="subheading">Work flow</span>
-            <h2 class="mb-3">How it works</h2>
-          </div>
-        </div>
-    		<div class="row">
-    			<div class="col-md-3 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services services-2">
-              <div class="media-body py-md-4 text-center">
-              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-route"></span></div>
-                <h3>Pick Destination</h3>
-                <p>A small river named Duden flows by their place and supplies it with you</p>
+            <div className='row'>
+              <div className='col-lg-3 col-md-12'>
+                <div className='pick-destination'><strong>Pick Destination</strong></div>
+                <div className='pick-destination-p'>A small river named Duden flows by their place and supplies it with you</div>
               </div>
-            </div>      
-          </div>
-          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services services-2">
-              <div class="media-body py-md-4 text-center">
-              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-select"></span></div>
-                <h3>Select Term</h3>
-                <p>A small river named Duden flows by their place and supplies it with you</p>
+              <div className='col-lg-3 col-md-12'>
+                <div className='select-term'><strong>Select Term</strong></div>
+                <div className='select-term'>A small river named Duden flows by their place and supplies it with you</div>
               </div>
-            </div>      
-          </div>
-          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services services-2">
-              <div class="media-body py-md-4 text-center">
-              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-rent"></span></div>
-                <h3>Choose A Car</h3>
-                <p>A small river named Duden flows by their place and supplies it with you</p>
+              <div className='col-lg-3 col-md-12'>
+                <div className='choose-a-car'><strong>Choose A Car</strong></div>
+                <div className='choose-a-car'>A small river named Duden flows by their place and supplies it with you</div>
               </div>
-            </div>      
-          </div>
-          <div class="col-md-3 d-flex align-self-stretch ftco-animate">
-            <div class="media block-6 services services-2">
-              <div class="media-body py-md-4 text-center">
-              	<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-review"></span></div>
-                <h3>Enjoy The Ride</h3>
-                <p>A small river named Duden flows by their place and supplies it with you</p>
+              <div className='col-lg-3 col-md-12'>
+                <div className='enjoy-the-ride'><strong>Enjoy The Ride</strong></div>
+                <div className='enjoy-the-ride'>A small river named Duden flows by their place and supplies it with you</div>
               </div>
-            </div>      
-          </div>
-    		</div>
-    	</div>
-    </section>
+            </div>
         </div>
     );
 };
